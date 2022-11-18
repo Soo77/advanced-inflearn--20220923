5a8533259ae00cdbbb7eecc2abe242c6903ea5c4
Soo77 <shimsh3@naver.com>
2022-11-19 02:23:40 +0900

    낼부터 다시 할것 , 갑자기 모두 에러떠서 gradle build 하니까 잘됨.

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -73,4 +73,26 @@ public class TemplateMethodTest {
     *
     * */
 
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        template1.excute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.excute();
+    }
+
+
+
+
 }
